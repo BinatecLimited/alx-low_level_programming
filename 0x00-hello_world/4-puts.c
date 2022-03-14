03bc75ad1128e338cadd695be041cef7ccc1dced
BinatecLimited <binateclimited894@gmail.com>
2022-03-14 06:41:55 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char string[55];
+	
+	strcpy(string, "\"Programming is like building a multilingual puzzle.\n");
+	puts(string);
+	return (0);
+}
